11e60534c6dfd3768a6a48d5e4b51bf3805c9efb
yusufmhmd4 <127377845+yusufmhmd4@users.noreply.github.com>
2023-12-09 10:18:40 +0530

    Added

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require("express")
+const app=express()
+
+app.listen(3000,()=>{
+    console.log("http://localhost:3000/")
+})
+
+app.get("/",(request,response)=>{
+    response.send("App is Running")
+})
