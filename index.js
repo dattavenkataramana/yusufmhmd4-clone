f97bd46f24ad26e24dbeb68e3642f89891e74853
yusufmhmd4 <127377845+yusufmhmd4@users.noreply.github.com>
2023-12-09 12:19:42 +0530

    added

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+const cors = require("cors");
+
+app.use(cors());
 
 const path = require("path");
 const { open } = require("sqlite");
